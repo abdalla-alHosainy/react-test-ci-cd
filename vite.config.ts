e411ae56eefd175dede2ac2813cda2778c860ff4
abdalla-alHosainy <abdallaelhosini@gmail.com>
2023-02-26 21:01:25 +0200

    Add smile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 console.log("Hi from vite.config.ts file 🙂");
-console.log("Env varaible: ", process.env.VITE_ENV_VAR);
+console.log("Env varaible: ", process.env.VITE_ENV_VAR + " 🙂");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
