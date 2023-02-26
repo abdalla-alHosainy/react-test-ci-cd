895c43095cdd6dabad19c16d8475125bb2d8588e
abdalla-alHosainy <abdallaelhosini@gmail.com>
2023-02-26 20:30:00 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+console.log("Hi from vite.config.ts file 🙂");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
